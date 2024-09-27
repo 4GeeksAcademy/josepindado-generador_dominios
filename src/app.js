a37c91798144f7c26aff2b2762cbea21c15bf9fd
Jose Pindado <joseangelpindado@gmail.com>
2024-09-27 18:30:58 +0000

    Firs commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#pronouns").innerHTML = printAll(pronouns);
+  document.querySelector("#adjs").innerHTML = printAll(adjs);
+  document.querySelector("#nouns").innerHTML = printAll(nouns);
+  document.querySelector("#extensions").innerHTML = printAll(extensions);
+  document.querySelector("#domains").innerHTML = generateDomain();
 };
+
+let pronouns = ["El", "Tu", "Su", "Mi"];
+let adjs = ["gran", "bueno", "malo", "feo"];
+let nouns = ["perro", "gato", "leon", "gorila"];
+let extensions = [".com", ".es", ".org", ".dev"];
+let spacing = "<br>";
+
+const generateDomain = () => {
+  let domain = "";
+  pronouns.forEach(pronoun => {
+    adjs.forEach(adj => {
+      nouns.forEach(noun => {
+        extensions.forEach(extension => {
+          domain += pronoun + adj + noun + extension + spacing;
+        });
+      });
+    });
+  });
+
+  return domain;
+};
+function printAll(pronouns) {
+  let result = "";
+  pronouns.forEach(element => {
+    result += element + "<br>";
+  });
+  return result;
+}
